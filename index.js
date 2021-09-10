49365131b782eae4796c304fef14c215a09d428c
Sneha Suman <36694331+sneha1102@users.noreply.github.com>
2021-09-10 12:01:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+const { MongoClient, ObjectID } = require("mongodb");
+const Express = require("express");
+const Cors = require("cors");
+const BodyParser = require("body-parser");
+const { request } = require("express");
+
+const client = new MongoClient(process.env["ATLAS_URI"]);
+const server = Express();
+
+server.use(BodyParser.json());
+server.use(BodyParser.urlencoded({ extended: true }));
+server.use(Cors());
+
+let collection;
+
+server.get("/search", async (request, response) => {
+    try {
+        let result = await collection.aggregate([
+            {
+                "$search": {
+                    "autocomplete": {
+                        "query": `${request.query.query}`,
+                        "path": "name",
+                        "fuzzy": {
+                            "maxEdits": 2,
+                            "prefixLength": 3
+                        }
+                    }
+                }
+            }
+        ]).toArray();
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.get("/get/:id", async (request, response) => {
+    try {
+        let result = await collection.findOne({ "_id": ObjectID(request.params.id) });
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.listen("3000", async () => {
+    try {
+        await client.connect();
+        collection = client.db("food").collection("recipes");
+    } catch (e) {
+        console.error(e);
+    }
+});
